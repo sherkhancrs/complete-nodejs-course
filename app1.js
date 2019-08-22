a11b88623e39505c22bab9738668ad26f77e5358
Sherkhan <sherkhancrs@gmail.com>
2019-08-22 16:19:54 +0600

    Global object

---
+++ app1.js
@@ -0,0 +1,16 @@
+// console.log() //global 
+
+// setTimeout();
+// clearTimeout();
+
+// setInterval();
+// clearInterval();
+
+var message = 'Conor McGregor is the best of the best';
+var text = "text";
+global.message = 'Nate Diaz is a real gangster';
+
+console.log(global.message);
+console.log(message);
+console.log(global.text);
+console.log(text);
