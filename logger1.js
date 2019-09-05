7739619e6e2de295c20a3981d7fe0db73ab811a0
Sherkhan <sherkhancrs@gmail.com>
2019-09-05 18:22:14 +0600

    Event Emitter

--- logger1.js
+++ logger1.js
@@ -1,11 +1,20 @@
+const EventEmitter = require('events');
+
+
 var url = 'http://mylogger.io.log';
 
+class Logger extends EventEmitter {
+  log(message) {
+    // Send an HTTP request;
+    console.log(message);
 
-function log(message) {
-  // Send an HTTP request;
-  console.log(message);
+    // Raise an event
+    this.emit('messageLogged', { id: 1, url: "https://"});
+  }
 }
 
 
-module.exports = log;
+
+
+module.exports = Logger;
 // console.log(module);
