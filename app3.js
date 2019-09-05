3aac8c04e1fee5cf698fd3cbd0e74fbfc569078d
Sherkhan <sherkhancrs@gmail.com>
2019-09-06 05:24:28 +0600

    http module

---
+++ app3.js
@@ -0,0 +1,20 @@
+ const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello World');
+    res.end();
+  }
+
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]))
+    res.end();
+  }
+})
+
+
+
+server.listen(3001);
+
+console.log('Listening on port 3001...')
+
